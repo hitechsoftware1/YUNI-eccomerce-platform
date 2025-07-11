4113445f0b0ae026a429532353af3c05ae076d23
Joel Lubega <joelhitech111@gmail.com>
2025-07-11 16:27:55 +0000

    why is it that when i put the images in through the dashboard they are n

--- src/next.config.ts
+++ src/next.config.ts
@@ -22,6 +22,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
